8805754814f7843748b8074a2dd8cc878b389ccc
arjundoshi221 <arjd221@gmail.com>
2024-02-22 18:50:33 +0530

    1.3.1

---
+++ arjun_doshi_level_1_HW_submission/1.3/1.3.1/1.3.1/MyFirstCProgram.cpp
@@ -0,0 +1,16 @@
+/*
+author: @arjundoshi221
+date: 02/22/24
+purpose: print name on screen
+version: 1.0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    // print the sentence
+    printf("My first C-program\nis a fact!\nGood, isn't it?");
+
+    return 0;
+}
